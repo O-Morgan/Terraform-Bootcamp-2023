4040d97a84beab64d0d68181239aab54960ad11d
Owen Morgan <owen.morgan7300@gmail.com>
2023-09-24 21:44:34 +0000

    #13 Terraform local state to terraform cloud using terraform cloud block

--- main.tf
+++ main.tf
@@ -1,12 +1,9 @@
 terraform {
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.17.0"
+  cloud {
+    organization = "Owen_Morgan"
+
+    workspaces {
+      name = "terra-house-of-funk"
     }
   }
 }
