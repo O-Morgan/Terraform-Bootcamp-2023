339d41b5ddf802a8e2533e968c7406ebae0d1dd0
Owen Morgan <96142367+Owen7300@users.noreply.github.com>
2023-09-28 22:35:15 +0100

    #23 Fixed and Restructured the Root Module migrated to local state from terraform cloud (#24)

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-terraform {
-  cloud {
-    organization = "Owen_Morgan"
+#terraform {
+#  cloud {
+#    organization = "Owen_Morgan"
 
-    workspaces {
-      name = "terra-house-of-funk"
-    }
-  }
-}
+#    workspaces {
+#      name = "terra-house-of-funk"
+#    }
+# }
+#}
 
 provider "aws" {
   # Configuration options
@@ -14,7 +14,8 @@ provider "aws" {
 
 provider "random" {
   # Configuration options
-}
+  }
+
 # Random string
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
@@ -31,10 +32,11 @@ resource "aws_s3_bucket" "example"{
 # Bucket Naming Rules
 # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html 
 bucket = random_string.bucket_name.result
-}
 
-output "random_bucket_name" {
- value = random_string.bucket_name.result
+
+tags = {
+    UserUuid = var.user_uuid
+  }
 }
 
 
