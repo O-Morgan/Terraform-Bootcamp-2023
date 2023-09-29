04921fcf7264f3def5a17a481dfe9e4d2f21bbab
Owen Morgan <96142367+Owen7300@users.noreply.github.com>
2023-09-29 12:27:49 +0100

    #25 replaced random bucket, edited variables, fixed configuration drift and imported resources to re-establish state file. (#26)

--- main.tf
+++ main.tf
@@ -1,37 +1,16 @@
-#terraform {
-#  cloud {
-#    organization = "Owen_Morgan"
 
-#    workspaces {
-#      name = "terra-house-of-funk"
-#    }
-# }
-#}
-
-provider "aws" {
-  # Configuration options
-}
-
-provider "random" {
-  # Configuration options
-  }
 
 # Random string
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
-resource "random_string" "bucket_name" {
-  lower            = true
-  upper            = false
-  length           = 32
-  special          = false
-  }
+
 
 # S3 bucket naming rules
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_metric
-resource "aws_s3_bucket" "example"{
+resource "aws_s3_bucket" "website_bucket" {
 
 # Bucket Naming Rules
 # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html 
-bucket = random_string.bucket_name.result
+bucket = var.s3_bucket_name
 
 
 tags = {
