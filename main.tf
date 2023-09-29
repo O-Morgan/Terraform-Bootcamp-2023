56b4a7803c485cf182f175498bc5ca3152c5203a
Owen Morgan <96142367+Owen7300@users.noreply.github.com>
2023-09-29 17:12:30 +0100

    #27 Refactor AWS bucket into terraform module (#28)

--- main.tf
+++ main.tf
@@ -1,22 +1,20 @@
+terraform {
+#  cloud {
+#    organization = "Owen_Morgan"
 
-
-# Random string
-# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
-
-
-# S3 bucket naming rules
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_metric
-resource "aws_s3_bucket" "website_bucket" {
-
-# Bucket Naming Rules
-# https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html 
-bucket = var.s3_bucket_name
-
-
-tags = {
-    UserUuid = var.user_uuid
+#    workspaces {
+#      name = "terra-house-of-funk"
+#    }
+# }
+#}
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs
   }
-}
+  
 
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.user_uuid
+  s3_bucket_name = var.s3_bucket_name
+}
 
   
---
+++ modules/terrahouse_aws/main.tf
@@ -0,0 +1,32 @@
+ terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+  # Configuration options
+
+
+# Random string
+# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
+
+
+# S3 bucket naming rules
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_metric
+resource "aws_s3_bucket" "website_bucket" {
+
+# Bucket Naming Rules
+# https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html 
+bucket = var.s3_bucket_name
+
+
+tags = {
+    UserUuid = var.user_uuid
+  }
+}
+
+
+  
