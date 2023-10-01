3b069bf95d0ba4610652f1d5bbf31bfed4de55d3
Owen Morgan <96142367+Owen7300@users.noreply.github.com>
2023-10-01 21:19:08 +0100

    #35 Only upload index.html if a content version has changes (#36)

--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -38,3 +38,13 @@ variable "error_html_filepath" {
     error_message = "The provided path for error.html does not exist"
   }
 }
+
+ variable "content_version" {
+  description = "Content Versioning"
+  type        = number
+  default     = 1
+  validation {
+    condition = var.content_version >= 1 && floor(var.content_version) == var.content_version
+    error_message = "Content version must be a positive integer starting at 1."
+  }
+}
