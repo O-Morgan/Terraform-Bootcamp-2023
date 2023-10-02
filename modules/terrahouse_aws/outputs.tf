c5e461f70bbab6e946895ed69058b2465891470a
Owen Morgan <96142367+Owen7300@users.noreply.github.com>
2023-10-02 10:56:58 +0100

    #38 Invalidate Cloudfront Stack when content version changes (#39)

--- modules/terrahouse_aws/outputs.tf
+++ modules/terrahouse_aws/outputs.tf
@@ -5,3 +5,7 @@ output "bucket_name" {
 output "website_endpoint" {
    value = aws_s3_bucket.website_bucket
 }
+
+output "cloudfront_url" {
+  value = aws_cloudfront_distribution.s3_distribution.domain_name
+}
