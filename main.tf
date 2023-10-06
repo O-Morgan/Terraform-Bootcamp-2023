4de241dfc7974b24a6a0d11d0bc703db723edb16
Owen Morgan <96142367+Owen7300@users.noreply.github.com>
2023-10-06 23:56:12 +0100

    #50 should be able to create run terraform provider block without errors (#51)

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 terraform {
+  required_providers {
+    terratowns = {
+      source = "local.providers/local/terratowns"
+      version = "1.0.0"
+    }
+  }
+
 #  cloud {
 #    organization = "Owen_Morgan"
 
@@ -10,15 +17,20 @@ terraform {
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs
   }
   
-
-module "terrahouse_aws" {
-  source = "./modules/terrahouse_aws"
-  user_uuid = var.user_uuid
-  s3_bucket_name = var.s3_bucket_name
-  index_html_filepath = "/workspace/terraform-beginner-bootcamp-2023/public/index.html"
-  error_html_filepath = "/workspace/terraform-beginner-bootcamp-2023/public/error.html"
-  content_version = var.content_version
-  assets_path = var.assets_path
+provider "terratowns" {
+  endpoint = "http//localhost:4567"
+  user_uuid = "e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
+  token = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  
 }
+#module "terrahouse_aws" 
+#  source = "./modules/terrahouse_aws"
+#  user_uuid = var.user_uuid
+#  s3_bucket_name = var.s3_bucket_name
+#  index_html_filepath = "/workspace/terraform-beginner-bootcamp-2023/public/index.html"
+#  error_html_filepath = "/workspace/terraform-beginner-bootcamp-2023/public/error.html"
+#  content_version = var.content_version
+#  assets_path = var.assets_path
+
 
   
